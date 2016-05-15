7eadf98c88885bd06df27a7a5cbc0396c5e5f722
spotterpong <spotterpong@gmail.com>
2016-05-15 11:10:09 +0100

    production issues

--- main.rb
+++ main.rb
@@ -36,44 +36,44 @@ def boucneBackCall(dist_id, startdate, enddate)
 	input_query = dist_id + " _index=mail"
 
 	params = {q: input_query, from: startdate + '+00:00', to: enddate +'+00:00', tz: 'UTC'}
-		puts startdate
-	puts enddate
 	r = session.get do |req|
 	  req.url 'logs/search'
 	  req.params = params
 	end
+	puts r.status
 	puts "\tDone!"
 
 
 	# Grab the interesting information from each item returned
 	puts "Parsing response..."
-	if r.body.length > 0 && r.status == 200
-		message_json_array = Array.new
-		r.body.each do |i|
-		  raw_message = JSON.parse(i["_raw"])
+	if r.body
+		if r.body.length > 0
+			message_json_array = Array.new
+			r.body.each do |i|
+			  raw_message = JSON.parse(i["_raw"])
 
-		  fields = raw_message["fields"] unless raw_message.nil?
-		  log_level = fields["level"] unless fields.nil?
+			  fields = raw_message["fields"] unless raw_message.nil?
+			  log_level = fields["level"] unless fields.nil?
 
-		  unless log_level.nil?
-		    if log_level.downcase == "info" && fields["method"].downcase == "logbouncedemail"
-		      # message_json = '{' + raw_message["message"].split('{')[0]
-		      # message_json = message_json.split('}')[0] + '}'
-		      message_json = raw_message["message"]
-		      message_json_array.push(message_json)
-		    end
-		  end
+			  unless log_level.nil?
+			    if log_level.downcase == "info" && fields["method"].downcase == "logbouncedemail"
+			      # message_json = '{' + raw_message["message"].split('{')[0]
+			      # message_json = message_json.split('}')[0] + '}'
+			      message_json = raw_message["message"]
+			      message_json_array.push(message_json)
+			    end
+			  end
+			end
+			message_json_json = {}
+			message_json_json[:messages] = message_json_array
+			message_json_json = JSON.generate(message_json_json)
+			return message_json_json
+		elsif r.body.length == 0
+			status = "Nothing found - API gave a return of #{r.status}"
+			puts status
+			return status
 		end
-		message_json_json = {}
-		message_json_json[:messages] = message_json_array
-		message_json_json = JSON.generate(message_json_json)
-		return message_json_json
-	elsif r.body.length == 0
-		status = "Nothing found - API gave a return of #{r.status}"
-		puts status
-		return status
 	end
-
 end
 
 
